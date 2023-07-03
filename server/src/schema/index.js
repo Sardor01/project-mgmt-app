6ac06192eec7d2118f6fdf832d50e3092ba43dc2
sardor01 <sardor.astanov.01@mail.ru>
2023-07-03 09:53:21 +0500

    fix: server on client delete projects delete bug

--- server/src/schema/index.js
+++ server/src/schema/index.js
@@ -97,7 +97,7 @@ const mutation = new GraphQLObjectType({
       },
       resolve(_, args) {
         Project.find({ clientId: args.id }).then((projects) => {
-          projects.forEach((project) => project?.remove())
+          projects.forEach((project) => project?.deleteOne())
         })
         return Client.findByIdAndRemove(args.id)
       },
